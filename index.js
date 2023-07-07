ab31e00bc1765eb8703b5300a07eaa0eb504dca1
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-07-06 21:30:50 -0400

    Se corrige error en app.set -> view Engine estaba con mayuscula

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const animeRoutes = require("./routes/anime");
 
-//express
-app.set("View engine", "hbs");
+// Configuración de Express
+app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Definimos la ruta de anime.js
+app.use("/anime", animeRoutes);
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
--- routes/anime.js
+++ routes/anime.js
@@ -2,9 +2,43 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
+//Página principal
 router.get("/", (req, res) => {
   const data = fs.readFileSync("./data/anime.json", "utf8");
   const animes = JSON.parse(data);
 
   res.render("index", { animes });
 });
+
+//Ruta y función para listar por ID:
+router.get("/:id", (req, res) => {
+  const data = fs.readFileSync("./data/anime.json", "utf8");
+  const animes = JSON.parse(data);
+
+  const anime = animes[id];
+
+  if (anime) {
+    res.json(anime);
+  } else {
+    res.status(404).json({ error: "No encontramos tu anime" });
+  }
+});
+
+//Ruta y función para buscar animes por nombre:
+router.get("/buscar/:nombre", (req, res) => {
+  const nombre = req.params.nombre.toLowerCase();
+
+  const data = fs.readFileSync("./data/anime.json", "utf8");
+  const animes = JSON.parse(data);
+
+  const anime = Object.values(animes).find(
+    (anime) => anime.nombre.toLowerCase() === nombre
+  ); //Comparamos el parametro nombre con el nombre de cada anime en el JSON
+  if (anime) {
+    res.json(anime);
+  } else {
+    res.status(404).json({ error: "No encontramos tu anime" });
+  }
+});
+
+module.exports = router;
